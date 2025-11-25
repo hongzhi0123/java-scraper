e65652e8050a9bd8de3810128b261a05227cf07d
hongzhi0123 <55428217+hongzhi0123@users.noreply.github.com>
2025-11-25 20:30:12 +0100

    simple structure of Properties & Services

--- src/main/java/com/example/eba/MyObject.java
+++ src/main/java/com/example/eba/MyObject.java
@@ -2,10 +2,10 @@ package com.example.eba;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
@@ -22,11 +22,34 @@ public class MyObject {
     @JsonProperty("EntityType")
     private String entityType;
 
-    @JsonProperty("Properties")
-    private List<Property> properties;
+    private Property property = new Property();
 
+    // Jackson calls this for the JSON array
+    @JsonProperty("Properties")
+    public void setProperties(List<Map<String, Object>> propertiesList) {
+        for (Map<String, Object> entry : propertiesList) {
+            for (Map.Entry<String, Object> keyValue : entry.entrySet()) {
+                property.setValue(keyValue.getKey(), keyValue.getValue());
+            }
+        }
+    }
+
+    private Map<String, List<String>> services = new HashMap<>();
+
+    // Jackson will call this for the JSON array
     @JsonProperty("Services")
-    private List<Service> services = new ArrayList<>();
+    public void setServices(List<Map<String, Object>> servicesList) {
+        for (Map<String, Object> entry : servicesList) {
+            for (Map.Entry<String, Object> keyValue : entry.entrySet()) {
+                var value = keyValue.getValue();
+                if (value instanceof List) {
+                    services.put(keyValue.getKey(), (List<String>) value);
+                } else if (value instanceof String) {
+                    services.put(keyValue.getKey(), Collections.singletonList((String) value)); 
+                }
+            }
+        }
+    }
 
     @JsonProperty("__EBA_EntityVersion")
     private String entityVersion;
@@ -34,71 +57,82 @@ public class MyObject {
 
 @Data
 class Property {
-    @JsonProperty("ENT_AUT")
     private List<String> autDate;
-
-    @JsonProperty("ENT_NAT_REF_COD")
     private String natRefCode;
-
-    @JsonProperty("ENT_NAM")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> name;
-
-    @JsonProperty("ENT_NAM_COM")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> nameCom;
-
-    @JsonProperty("ENT_ADD")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> address;
-
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
-    @JsonProperty("ENT_TOW_CIT_RES")
     private List<String> city;
-
-    @JsonProperty("ENT_COU_RES")
     private String country;
-
-    @JsonProperty("ENT_POS_COD")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> postalCode;
-
-    @JsonProperty("ENT_EXC")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> excl;
-
-    @JsonProperty("ENT_DES_ACT_EXC_SCP")
-    @JsonDeserialize(using = SingleOrListDeserializer.class)
     private List<String> excDes;
-
-    @JsonProperty("ENT_TYP_PAR_ENT")
     private String parentType;
-
-    @JsonProperty("ENT_COD_PAR_ENT")
     private String parentCode;
-
-    @JsonProperty("DER_CHI_ENT_AUT")
     private String status;
-    
-}
 
-@Data
-class Service {
-    private String country;
-    private List<String> serviceCodes;
-
-    @JsonAnySetter
-    public void setService(String key, Object value) {
-        this.country = key;
-        this.serviceCodes = new ArrayList<>();
-
-        if (value instanceof List) {
-            // Handle array like ["PS_070", "PS_080"]
-            this.serviceCodes.addAll((List<String>) value);
-        } else if (value instanceof String) {
-            // Handle single value like "PS_060"
-            this.serviceCodes.add((String) value);
+    // Called by MyObject to flatten the array
+    public void setValue(String key, Object value) {
+        switch (key) {
+            case "ENT_AUT":
+                this.autDate = normalizeList(value);
+                break;
+            case "ENT_NAT_REF_COD":
+                this.natRefCode = (String) value;
+                break;
+            case "ENT_NAM":
+                this.name = normalizeList(value);
+                break;
+            case "ENT_NAM_COM":
+                this.nameCom = normalizeList(value);
+                break;
+            case "ENT_ADD":
+                this.address = normalizeList(value); // Handles string or array
+                break;
+            case "ENT_TOW_CIT_RES":
+                this.city = normalizeList(value); // Handles string or array
+                break;
+            case "ENT_COU_RES":
+                this.country = (String) value;
+                break;
+            case "ENT_POS_COD":
+                this.postalCode = normalizeList(value);
+                break;
+            case "ENT_EXC":
+                this.excl = normalizeList(value);
+                break;
+            case "ENT_DES_ACT_EXC_SCP":
+                this.excDes = normalizeList(value);
+                break;
+            case "ENT_TYP_PAR_ENT":
+                this.parentType = (String) value;
+                break;
+            case "ENT_COD_PAR_ENT":
+                this.parentCode = (String) value;
+                break;
+            case "DER_CHI_ENT_AUT":
+                this.status = (String) value;
+                break;
         }
     }
 
+    // Normalizes both "value" and ["value"] to List<String>
+    private List<String> normalizeList(Object value) {
+        List<String> list = new ArrayList<>();
+        if (value == null)
+            return list;
+
+        if (value instanceof String str) {
+            list.add(str);
+        } else if (value instanceof List<?> lst) {
+            for (Object item : lst) {
+                if (item != null) {
+                    list.add(item.toString());
+                }
+            }
+        }
+        return list;
+    }
 }
+
+
